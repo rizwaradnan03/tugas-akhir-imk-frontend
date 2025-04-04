551e228242610c96637b660864e1699ba560b7fe
RISKYR1212 <rizkyrispaldi0@gmail.com>
2025-04-04 21:00:32 +0700

    malem asyik

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { BsCart3 } from 'react-icons/bs'
 
 const Navbar = () => {
+  const [cartCount, setCartCount] = useState(0) 
+
   return (
-    
-    <div>
-        
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Beranda</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <Link className="nav-link active" aria-current="page" to="/category">Kategori</Link>
-        {/* <a className="nav-link" href="#">Produk</a> */}
-        <Link className="nav-link" to="/product">product</Link>
-        <Link className="nav-link" to="/Checkout">Checkout</Link>
-        
+    <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
+      <div className="container-fluid">
+        <Link className="navbar-brand fw-bold" to="/">Beranda</Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav me-auto">
+            <Link className="nav-link" to="/category">Kategori</Link>
+            <Link className="nav-link" to="/product">Produk</Link>
+            <Link className="nav-link" to="/checkout">Checkout</Link>
+          </div>
+
+          {/* Ikon Keranjang */}
+          <div className="d-flex align-items-center">
+            <Link to="/cart" className="text-decoration-none position-relative">
+              <BsCart3 size={28} style={{ color: 'orange' }} />
+              {cartCount > 0 && (
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  {cartCount}
+                </span>
+              )}
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
-
-
-        
-    </div>
+    </nav>
   )
 }
 
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -2,37 +2,74 @@ import React from 'react'
 
 const Home = () => {
   return (
-
     <div>
-      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="https://img.freepik.com/free-vector/hand-drawn-person-playing-padel-illustration_23-2149208677.jpg?t=st=1743759340~exp=1743762940~hmac=c65e48305ca6eecf485f393a98127fa4ac3636b416935afd356074873b89a57e&w=1380" className="d-block w-100" alt="..." />
+      <div className="position-relative">
+        {/* COURASEL FREEPIK */}
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="https://img.freepik.com/free-vector/hand-drawn-person-playing-padel-illustration_23-2149208677.jpg?t=st=1743759340~exp=1743762940~hmac=c65e48305ca6eecf485f393a98127fa4ac3636b416935afd356074873b89a57e&w=1380"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://img.freepik.com/free-vector/hand-drawn-person-playing-padel-illustration_23-2149208677.jpg?t=st=1743759340~exp=1743762940~hmac=c65e48305ca6eecf485f393a98127fa4ac3636b416935afd356074873b89a57e&w=1380"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://img.freepik.com/free-vector/hand-drawn-person-playing-padel-illustration_23-2149208677.jpg?t=st=1743759340~exp=1743762940~hmac=c65e48305ca6eecf485f393a98127fa4ac3636b416935afd356074873b89a57e&w=1380"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
           </div>
-          <div className="carousel-item">
-            <img src="https://img.freepik.com/free-vector/hand-drawn-person-playing-padel-illustration_23-2149208677.jpg?t=st=1743759340~exp=1743762940~hmac=c65e48305ca6eecf485f393a98127fa4ac3636b416935afd356074873b89a57e&w=1380" className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="https://img.freepik.com/free-vector/hand-drawn-person-playing-padel-illustration_23-2149208677.jpg?t=st=1743759340~exp=1743762940~hmac=c65e48305ca6eecf485f393a98127fa4ac3636b416935afd356074873b89a57e&w=1380" className="d-block w-100" alt="..." />
-          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+
+        {/* TEKS DI TENGAH COURASEL */}
+        <div
+          className="position-absolute top-50 start-50 translate-middle text-Dark text-center text-lg-start"
+          style={{ zIndex: 10  }} 
+        >
+          <h1 className="bg-dark bg-opacity-50 p-2 rounded fw ">
+            RIZKY SPORTS 
+          </h1>
+        </div>
       </div>
-      <img src="..." className="object-fit-contain border rounded" alt="..." />
-          <img src="https://img.freepik.com/premium-photo/basketball_172429-282.jpg?uid=R194703580&ga=GA1.1.1203588368.1739245046&semt=ais_hybrid&w=740" width={200} height={150} className="object-fit-cover border rounded" alt="Bola Basket" />
-          <img src="..." className="object-fit-fill border rounded" alt="..." />
-          <img src="..." className="object-fit-scale border rounded" alt="..." />
-          <img src="..." className="object-fit-none border rounded" alt="..."></img>
-          <img src="..." className="object-fit-contain border rounded" alt="..." />
 
-
+      {/* GAMBAR GAMBARAN */}
+      <div className="mt-4 d-flex gap-2 flex-wrap">
+        <img
+          src="https://img.freepik.com/premium-photo/basketball_172429-282.jpg?uid=R194703580&ga=GA1.1.1203588368.1739245046&semt=ais_hybrid&w=740"
+          width={200}
+          height={150}
+          className="object-fit-cover border rounded"
+          alt="Bola Basket"
+        />
+      </div>
     </div>
   )
 }
